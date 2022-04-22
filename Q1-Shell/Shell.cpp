3f81064a74d0a00c4fc9fd1029d6da95bdef4eb7
Sourabh Yelluru <sourabhyelluru@gmail.com>
2022-04-23 00:19:00 +0530

    Executable file path done

--- Q1-Shell/Shell.cpp
+++ Q1-Shell/Shell.cpp
@@ -1,10 +1,13 @@
 #include <cassert>
 #include <iostream>
+#include <cstring>
+#include <unistd.h>
+#define PATH_MAX 256
 using namespace std;
 
 namespace ShellOperations
 {
-    char** splitString(char* input, int &size, char delim = ' ')
+    const char** splitString(char* input, int &size, char delim = ' ')
     {
         char* c = input;
         char* end = nullptr;
@@ -23,7 +26,7 @@ namespace ShellOperations
 
         int argv_index = 0;
 
-        char** result = new char*[size];
+        const char** result = new const char*[size];
 
         while (c != end)
         {
@@ -43,6 +46,61 @@ namespace ShellOperations
 
         return result;
     }
+
+    const char* getExecutablePath(const char* execName)
+    {
+        if (!execName)
+            return nullptr;
+
+        // Case 1: When absolute file path is already given
+        if (*execName == '/')
+        {
+            char* ret = new char[strlen(execName) + 1];
+            strcpy(ret, execName);
+            return ret;
+        }
+
+        // Case 2: When we need to search for location in PATH env variable
+        const char* tmp = getenv("PATH");
+        char* env_path = new char[strlen(tmp) + 1];
+        strcpy(env_path, tmp);
+
+        int count = 0;
+        const char** locs = splitString(env_path, count, ':');
+
+        char* complete_path = new char[PATH_MAX + 1];
+        
+        // Append current file loc to all paths and check if such file exists
+        for (int i = 0; i < count; ++i)
+        {
+            int len = strlen(locs[i]);
+            
+            strcpy(complete_path, locs[i]);
+            complete_path[len] = '/';
+            strcpy(complete_path + len + 1, execName);
+
+            if (access(complete_path, F_OK | X_OK) != 0)
+                continue;
+
+            delete[] env_path;
+            delete[] locs;
+            return complete_path;
+        }
+
+        // deallocate resources
+        delete[] env_path;
+        delete[] locs;
+
+        // Case 3: Find file w.r.t. relative pos
+        realpath(execName, complete_path);
+
+        if (access(complete_path, F_OK | X_OK) == 0)
+            return complete_path;
+
+        delete[] complete_path;
+
+        return nullptr;
+    }
 }
 
 namespace ShellOperationTester
@@ -61,12 +119,24 @@ namespace ShellOperationTester
 
         delete[] res;
     }
+    void testGetPath()
+    {
+        auto a = ShellOperations::getExecutablePath("ls");
+        cout << a << endl;
+        delete[] a;
+
+        cout << (a = ShellOperations::getExecutablePath("wc")) << endl;
+        delete[] a;
+
+        cout << (a = ShellOperations::getExecutablePath("./shell.o")) << endl;
+        delete[] a;
+    }
 }
 
 int main()
 {
-    ShellOperationTester::testSplitString();
+    ShellOperationTester::testGetPath();
 	cout << "Done" << endl;
-
+    
     return 0;
 }
